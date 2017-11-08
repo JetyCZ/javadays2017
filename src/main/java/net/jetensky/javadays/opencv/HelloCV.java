6d8e707fff706e8665547c086546c3b3f34bd4b3
Pavel Jetensky <pavel.jetensky@hooyu.com>
2017-11-08 21:37:37 +0100

    Saving and viewing image

--- src/main/java/net/jetensky/javadays/opencv/HelloCV.java
+++ src/main/java/net/jetensky/javadays/opencv/HelloCV.java
@@ -2,13 +2,17 @@ package net.jetensky.javadays.opencv;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class HelloCV {
 
     public static void main(String[] args){
         System.load("/usr/share/OpenCV/java/libopencv_java320.so");
-        Mat mat = Mat.eye(new Size(3,3), CvType.CV_8U);
-        System.out.println(mat.dump());
+        Mat mat = Mat.eye(new Size(300,300), CvType.CV_8U);
+        mat.setTo(new Scalar(128));
+        // UIUtil.showWindow(mat);
+        Imgcodecs.imwrite("/tmp/a.png", mat);
     }
 }
---
+++ src/main/java/net/jetensky/javadays/opencv/UIUtil.java
@@ -0,0 +1,43 @@
+package net.jetensky.javadays.opencv;
+
+import org.opencv.core.Mat;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+
+/**
+ * This is not class to be used in production, but during development phase while debugging image processing
+ */
+public class UIUtil {
+    public static void showWindow(Mat mat) {
+        BufferedImage bufferedImage = matToBufferedImage(mat);
+        showWindow(bufferedImage);
+    }
+
+    public static BufferedImage matToBufferedImage(Mat frame) {
+        int type = 0;
+        if (frame.channels() == 1) {
+            type = BufferedImage.TYPE_BYTE_GRAY;
+        } else if (frame.channels() == 3) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        BufferedImage image = new BufferedImage(frame.width(), frame.height(), type);
+        WritableRaster raster = image.getRaster();
+        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+        byte[] data = dataBuffer.getData();
+        frame.get(0, 0, data);
+
+        return image;
+    }
+
+    public static void showWindow(BufferedImage img)  {
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+        frame.setSize(img.getWidth(), img.getHeight() + 30);
+        frame.setTitle("Image " + img.getWidth() + "x" + img.getHeight() + ", type=" + img.getType());
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+}
